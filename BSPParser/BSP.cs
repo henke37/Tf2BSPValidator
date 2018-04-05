6ac188a0aa25e93330c15c99ab47ab7ee9374a78
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2018-04-05 15:25:22 +0200

    Initial commit

---
+++ BSPParser/BSP.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSPParser
+{
+    public class BSP {
+        private const string FILE_SIGNATURE="VBSP";
+
+        public BSP(Stream stream) {
+            Parse(stream);
+        }
+
+        public void Parse(Stream stream) {
+            using(BinaryReader r=new BinaryReader(stream)) {
+                string sig = new string(r.ReadChars(4));
+                if(sig != FILE_SIGNATURE) throw new ArgumentException("That's not a BSP file!");
+            }
+        }
+    }
+}
--- BSPValidator/Program.cs
+++ BSPValidator/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BSPParser;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +9,13 @@ using System.Threading.Tasks;
 namespace BSPValidator {
     class Program {
         static void Main(string[] args) {
+            if(args.Length!=1) {
+                Console.WriteLine("One argument, the file to validate.");
+                return;
+            }
+
+            Stream s = File.OpenRead(args[1]);
+            BSP bsp = new BSP(s);
         }
     }
 }
