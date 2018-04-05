c33b4d2553a09dc822be2feb780d050d5521efd1
Henrik "Henke37" Andersson <henke@henke37.cjb.net>
2018-04-05 16:47:59 +0200

    Refactor the validator code to use an object to store state in.

--- BSPValidator/Program.cs
+++ BSPValidator/Program.cs
@@ -7,15 +7,40 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BSPValidator {
-    class Program {
+    class BSPValidator {
+        private const uint TF2_BSP_VERSION=20;
+
+        private BSP bsp;
+
         static void Main(string[] args) {
-            if(args.Length!=1) {
+            if(args.Length != 1) {
                 Console.WriteLine("One argument, the file to validate.");
                 return;
             }
 
-            Stream s = File.OpenRead(args[0]);
-            BSP bsp = new BSP(s);
+            var v = new BSPValidator(args[0]);
+            v.Validate();
+        }
+
+        public BSPValidator(string filename) {
+            Init(File.OpenRead(filename));
+        }
+
+        private BSPValidator(Stream stream) {
+            Init(stream);
+        }
+
+        private void Init(Stream stream) {
+            bsp = new BSP(stream);
+        }
+
+        private void Validate() {
+            if(bsp.version != TF2_BSP_VERSION) error("Wrong BSP Version");
+
+        }
+
+        private void error(string msg) {
+            Console.WriteLine(msg);
         }
     }
 }
